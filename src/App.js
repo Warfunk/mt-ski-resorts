330c2a8625a8045403218b449851c68e2b25eb3f
unknown <spenceaw@gmail.com>
2021-02-09 13:43:02 -0500

    darker box shadow

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ const App = () => {
         {state.route === 'register' || state.route === 'signin' ? (
           <Fields route={state.route} dispatch={dispatch} />
         ) : (
-          <div className='shadow-5 main'>
+          <div className='shadow-3 main'>
             {state.showResorts ? <ResortList dispatch={dispatch} /> : null}
             <CompareSnow snow={state.resortCompare} />
             {saveUi}
